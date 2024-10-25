7b82be34e61434cb555fc16634809effc38bcaab
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-25 18:10:04 -0400

    Add base image conversion functionality
    Add conversion from image filetypes through Pillow library

--- conversion/main.py
+++ conversion/main.py
@@ -1,12 +1,18 @@
-from hashlib import sha256
+import os
 
-def get_sha256(unencoded: str) -> str:
-    return(sha256(unencoded.encode()).hexdigest()) 
+from PIL import Image
 
+class ConversionApp:
+    def img_convert(self, input_file: str, output_file: str) -> None:
+        input_image: Image = Image.open(input_file)
+        input_image.convert('RGB').save(output_file)
 def main():
-    unencodedString = input("Enter unencoded string: ")
-    encodedString = get_sha256(unencodedString)
-    print(encodedString)
+    conversion_app = ConversionApp()
+    input_file = input("Please enter image file name: ")
+    if not os.path.isfile(input_file):
+        raise FileNotFoundError("File does not exist")
+    output_file = input("Please enter output image file name: ")
+    conversion_app.img_convert(input_file, output_file)
 
 if __name__ == "__main__":
     main()
